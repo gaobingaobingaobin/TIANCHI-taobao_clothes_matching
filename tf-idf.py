e7074521a06cd6a61cfcbe2c08e411c5c45a5c52
AndyShan <sgd1995@163.com>
2016-08-06 22:40:25 +0800

    add item label

--- tf-idf.py
+++ tf-idf.py
@@ -14,6 +14,7 @@ def tfidf(corpus):
     vectorizer = CountVectorizer()
     x = vectorizer.fit_transform(corpus)
     a = x.toarray()
+    print vectorizer.get_feature_names()
     transformer = TfidfTransformer()
     tfidf = transformer.fit_transform(a)
     return tfidf.toarray()
@@ -25,3 +26,29 @@ def euclidean_metric(vector1,vector2):
     sqDistance = sqDiffVector.sum()
     distance = sqDistance ** 0.5
     return distance
+
+def classification():
+    f = open("dataset/raw/dim_items.txt", "r")
+    label = {}
+    for line in f:
+        arr = line.replace("\n","").split(" ")
+        label[arr[0]] = arr[1]
+    return label
+
+def item_matcher_pair():
+    f = open("dataset/raw/dim_fashion_matchsets.txt", "r")
+    label = classification()
+    matcher_pair = []
+    index = 0
+    for line in f:
+        if index == 3:
+            break
+        index += 1
+        p = []
+        arr = line.replace("\n","").split(" ")
+        pairs = arr[1].split(";")
+        for pair in pairs:
+            item = pair.split(",")[0]
+            p.append(label[item])
+        matcher_pair.append(p)
+    return matcher_pair
